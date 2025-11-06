2c3bfdf579e345ea700b436857f31605fbef30b2
OlgierdIwaniuk <oloiwani@gmail.com>
2025-11-06 21:45:15 +0100

    zejsciówka3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int ileprzewiezionego=0;
+    unsigned int ilezeglowcow=0;
+    while(ileprzewiezionego<towar)
+    {
+        Statek* statek = stocznia(); 
+        ileprzewiezionego+= statek->transportuj();
+        if (dynamic_cast<Zaglowiec*>(statek))
+        {
+            ++ilezeglowcow;
+        }
+        delete statek;
+    }
+    return ilezeglowcow;
 }
